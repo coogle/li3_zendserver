f268e6c1ca9a7a0a8bf4dff73365f60adb1a7f8b
John Coggeshall <john@coggeshall.org>
2013-03-20 20:13:08 -0400

    Adding methods to generate Debug and Profile URLs

--- ZendServer.php
+++ ZendServer.php
@@ -2,6 +2,10 @@
 
 namespace li3_zendserver;
 
+use lithium\core\Environment;
+use lithium\net\http\Router;
+use lithium\action\Request;
+
 class ZendServer extends \lithium\core\Adaptable
 {
 	/**
@@ -11,4 +15,51 @@ class ZendServer extends \lithium\core\Adaptable
 	 */
 	protected static $_configurations = array();
 	
+	static protected function _generateBaseDebugUrl(Request $request, $options = array()) {
+		$request = clone $request;
+		$url = Router::match($request->params, $request, array('absolute' => true));
+		
+		$config = static::config(Environment::get());
+		
+		if(!isset($config['debugger']) || !isset($config['debugger']['host'])) {
+			throw new \Exception("You must provide a debugger.host value");
+		}
+		
+		$debugger_host = $config['debugger']['host'];
+		$debugger_port = isset($config['debugger']['port']) ? $config['debugger']['port'] : 10137;
+		
+		return $url . "?" .  http_build_query(array(
+			'start_debug' => 1,
+			'debug_host' => $debugger_host,
+			'debug_port' => $debugger_port
+		));
+	}
+	
+	static public function generateProfilerUrl(Request $request, $options = array()) {
+		
+		$default = array(
+			'start_profile' => 1,
+			'debug_coverage' => 1,
+			'debug_fastfile' => 1,
+		);
+		
+		$baseUrl = static::_generateBaseDebugUrl($request, $options);
+		
+		$options += $default;
+		return $baseUrl . "&" . http_build_query($options);
+	}
+	
+	static public function generateDebugUrl(Request $request, $options = array()) {
+		$default = array(
+			'debug_fastfile' => 1,
+			'debug_stop' => 1,
+			'use_ssl' => 1,
+			'no_remote' => 1,
+		);
+		
+		$baseUrl = static::_generateBaseDebugUrl($request, $options);
+		
+		$options += $default;
+		return $baseUrl . "&" . http_build_query($options);
+	}
 }
